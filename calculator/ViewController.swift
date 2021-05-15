95754a9a9da640a8fb273e0552ce813ac3ab693b
Gerardo <72542243+Gerardo-cpr@users.noreply.github.com>
2021-05-15 14:52:06 -0700

    Initial commit

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func numberSelected(_ sender: UIButton) {
+        if displayLabel.text!.starts(with: "0") {
+            displayLabel.text!.remove(at: displayLabel.text!.startIndex)
+        }
+        displayLabel.text! += sender.title(for: UIControl.State.normal)!
+    }
+    @IBAction func clearDisplay (_ sender: UIButton) {
+        displayLabel.text = "0"
+    }
+    @IBAction func calculate(_ sender: UIButton) {
+        
+        let expression = parseInput(displayLabel.text!);
+        let result = NSExpression(format: expression).expressionValue(with: nil, context: nil) as! Double
+        displayLabel.text = String(result)
+    }
+    @IBAction func operationSelected (_ sender: UIButton) {
+        displayLabel.text! += sender.title(for: UIControl.State.normal)!
+    }
+    
+    func parseInput(_ input: String) -> String {
+        var parseInput = input
+        
+        parseInput = parseInput.replacingOccurrences(of: "x", with: "*")
+        parseInput = parseInput.replacingOccurrences(of: "÷", with: "/")
+        return parseInput
+    }
+    
 }
 
